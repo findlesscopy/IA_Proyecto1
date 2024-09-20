202eb5c22bce744f39b7360454388821d5d98bd6
José Ibarra <3007542210101@ingenieria.usac.edu.gt>
2024-09-19 23:42:38 -0600

    [ADD] ingreso y clicks

---
+++ index.js
@@ -0,0 +1,36 @@
+const robot = require('@hurdlegroup/robotjs');
+const { exec } = require('child_process');
+
+// Función para hacer una pausa
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Función para mover el ratón con pausa y clic
+async function performMouseActions() {
+    // Abre Chrome en la página del juego en Windows
+    exec('start Brave http://en.battleship-game.org/');
+
+    // Mover el ratón a (400, 700)
+    await sleep(1000);
+    robot.moveMouse(400, 700);
+    console.log("Mouse moved to (400, 700)");
+
+    // Hacer clic
+    await sleep(1000);
+    robot.mouseClick();
+    console.log("Mouse clicked at (400, 700)");
+
+    // Mover el ratón a (800, 300)
+    await sleep(1000);
+    robot.moveMouse(900, 400);
+    console.log("Mouse moved to (1000, 380)");
+
+    // Hacer clic
+    await sleep(1000);
+    robot.mouseClick();
+    console.log("Mouse clicked at (1000, 380)");
+}
+
+// Ejecutar las acciones del ratón
+performMouseActions();
